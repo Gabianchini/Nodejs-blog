2ac018c0c34ccf08be17fd0a954ea490cc5f1a4c
Gabianchini <gabibisdx@gmail.com>
2024-03-07 18:59:56 -0300

    Connect to mongodb

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 require('dotenv').config();
 
-const express = require('express')
-const expressLayout = require('express-ejs-layouts')
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+
+const connectDB = require('./server/config/db');
 
 const app = express();
 const PORT = 5000 || process.env.PORT;
+
+//Connect db
+connectDB();
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 app.use(express.static('public'));
 
