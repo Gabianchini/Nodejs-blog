6e1c4e2056ecb45551886c4fc37b13b96288b810
Gabianchini <gabibisdx@gmail.com>
2024-03-11 18:23:55 -0300

    create admin page and features login and register user

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
-
+const session = require('express-session');
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo');
 
 const connectDB = require('./server/config/db');
 
@@ -13,6 +15,16 @@ connectDB();
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(session({
+ secret: 'keyboard cat',
+ resave: false,
+ saveUnitialized: true,
+ store: MongoStore.create({
+    mongoUrl:process.env.MONGODB_URI
+ }),
+}));
 
 app.use(express.static('public'));
 
@@ -22,6 +34,7 @@ app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
 app.use('/', require('./server/routes/main'));
+app.use('/', require('./server/routes/admin'));
 
 app.listen(PORT, () =>{
 console.log(`App is listening to port ${PORT}`)
